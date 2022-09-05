f6e1d2d4a78e82373c866731c3f863a4c8384255
Artica2012 <schneiaj@gmail.com>
2022-09-05 08:02:17 -0700

    - Started on the initiative tracker. Working on tracker and database
    Note:
     - Currently database is only holding Disease, Feat, Item and Power info.  Will implement search and bot functions before completing the rest of the parsing.
    
    TODO:
    Switch to Postgresql from SQLite
    Initiative Tracker
    Improved Bot Search
    Parers for rest of data
    Scrape HTML data

--- database_models.py
+++ database_models.py
@@ -4,12 +4,14 @@ from sqlalchemy import Integer
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
+import sqlalchemy as db
 
 import discord
 
 Base = declarative_base()
 
 class TrackerTable(Base):
+    #name schema is Tracker_{guild_id}
     __abstract__ = True
 
     id = Column(Integer, autoincrement=True, primary_key=True)
@@ -24,16 +26,19 @@ class TrackerTable(Base):
 
 
 class ConditionTable(Base):
+    # name schema os Condition_{guild_id}
     __abstract__ = True
+    __guild_id__ = ''
+
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    character_id = Column(Integer, ForeignKey("Tracker.id"))  # Foreign Key
+    character_id = Column(Integer, ForeignKey(f"Tracker_{__guild_id__}.id"))  # Foreign Key
     condition = Column(String(255), nullable=False)
     duration = Column(Integer)
     beginning = Column(Boolean, default=False)
 
     # relationships
-    tracker = relationship("TrackerTable", backref="ConditionTable")
+    tracker = relationship(f"Tracker_{__guild_id__}", backref=f"Condition_{__guild_id__}")
 
 
 class Global(Base):
@@ -47,12 +52,13 @@ def get_guild_tracker(server: discord.Guild):
     guild_id = server.id
     tablename = f"{guild_id}"
     class_name = f"Tracker_{guild_id}"
-    Model = type(class_name, (TrackerTable,),{'__tablename__':tablename})
+    Model = type(class_name, (TrackerTable,),{'__tablename__':tablename, 'guild_id': guild_id })
     return Model
 
 def get_guild_conditions(server:discord.Guild):
     guild_id = server.id
     tablename = f"{guild_id}"
     class_name = f"Condition_{guild_id}"
-    Model = type(class_name, (ConditionTable,),{'__tablename__':tablename})
+    Model = type(class_name, (ConditionTable,),{'__tablename__':tablename, 'guild_id': guild_id})
     return Model
+
--- initiative.py
+++ initiative.py
@@ -5,6 +5,9 @@
 import discord
 from discord.ext import commands
 from discord.commands import SlashCommandGroup
+
+from sqlalchemy import insert
+import database_models
 import database_operations
 import sqlite3
 import sqlalchemy as db
@@ -14,7 +17,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from sqlalchemy import create_engine
 from database_models import Global, ConditionTable, TrackerTable, Base
-from sqlalchemy.orm import Session,Query
+from sqlalchemy.orm import Session, Query
 from sqlalchemy import select
 
 import os
@@ -41,22 +44,59 @@ id | time | initiative
 
 '''
 
+
 # Function
 
-# Set up the tracker if it does not exit.
+#TODO - FINISH THIS
+def tracker_table(server: discord.Guild, metadata):
+    tablename = f"Tracker_{server.id}"
+    emp = db.Table(tablename, metadata,
+                   db.Column('id', db.INTEGER(), autoincrement=True, primary_key=True),
+                   db.Column('name', db.String(255), nullable=False, unique=True),
+                   db.Column('init', db.INTEGER(), default=0),
+                   db.Column('player', db.BOOLEAN, default=False),
+                   db.Column('user', db.INTEGER(), nullable=False),
+                   db.Column('current_hp', db.INTEGER(), default=0),
+                   db.Column('max_hp', db.INTEGER(), default=1),
+                   db.Column('temp_hp', db.INTEGER(), default=0),
+                   )
+
+
+# Set up the tracker if it does not exit.db
 def setup_tracker(server: discord.Guild):
     try:
         engine = create_engine(f'sqlite:///{SERVER_DATA}.db', future=True)
+        # engine = create_engine('postgresql://')
+        conn = engine.connect()
+        metadata = db.MetaData()
+        ID = server.id
+        emp = db.Table(f"Tracker_{ID}", metadata,
+                       db.Column('id', db.INTEGER(), autoincrement=True, primary_key=True),
+                       db.Column('name', db.String(255), nullable=False, unique=True),
+                       db.Column('init', db.INTEGER(), default=0),
+                       db.Column('player', db.BOOLEAN, default=False),
+                       db.Column('user', db.INTEGER(), nullable=False),
+                       db.Column('current_hp', db.INTEGER(), default=0),
+                       db.Column('max_hp', db.INTEGER(), default=1),
+                       db.Column('temp_hp', db.INTEGER(), default=0),
+                       )
+        con = db.Table(f"Condition_{ID}", metadata,
+                       db.Column('id', db.INTEGER(), autoincrement=True, primary_key=True),
+                       db.Column('character_id', db.INTEGER(), ForeignKey(f'Tracker_{ID}.id')),
+                       db.Column('condition', db.String(255), nullable=False),
+                       db.Column('duration', db.INTEGER()),
+                       db.Column('beginning', db.BOOLEAN, default=False)
+                       )
+        metadata.create_all(engine)
         Base.metadata.create_all(engine)
 
         with Session(engine) as session:
             guild = Global(
-                guild_id = server.id,
-                time = 0,
+                guild_id=server.id,
+                time=0,
             )
             session.add(guild)
             session.commit()
-
 
         return True
     except Exception as e:
@@ -65,54 +105,108 @@ def setup_tracker(server: discord.Guild):
 
 
 # Add a player to the database
-def add_player(name: str, user: int, server: discord.Guild):
+def add_player(name: str, user: int, server: discord.Guild, HP: int):
     engine = create_engine(f'sqlite:///{SERVER_DATA}.db', future=True)
+    tablename = f'Tracker_{server.id}'
+    metadata = db.MetaData()
     try:
-        with Session(engine) as session:
-            character = TrackerTable(
-                name = name,
-                init = 0,
-                player = True,
-                user = user,
-                guild = server.id
-
-            )
-            session.add(character)
-            session.commit()
-            return True
+        emp = db.Table(tablename, metadata,
+                       db.Column('id', db.INTEGER(), autoincrement=True, primary_key=True),
+                       db.Column('name', db.String(255), nullable=False, unique=True),
+                       db.Column('init', db.INTEGER(), default=0),
+                       db.Column('player', db.BOOLEAN, default=False),
+                       db.Column('user', db.INTEGER(), nullable=False),
+                       db.Column('current_hp', db.INTEGER(), default=0),
+                       db.Column('max_hp', db.INTEGER(), default=1),
+                       db.Column('temp_hp', db.INTEGER(), default=0),
+                       )
+        stmt = emp.insert().values(
+            name=name,
+            init=0,
+            player=True,
+            user=user,
+            current_hp=HP,
+            max_hp=HP,
+            temp_hp=0
+        )
+        compiled = stmt.compile()
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            conn.commit()
+        return True
     except Exception as e:
+        print(e)
         return False
+
 
 # Add an NPC to the database
-def add_npc(name: str, user: int, server: discord.Guild):
+def add_npc(name: str, user: int, server: discord.Guild, HP: int):
     engine = create_engine(f'sqlite:///{SERVER_DATA}.db', future=True)
+    tablename = f'Tracker_{server.id}'
+    metadata = db.MetaData()
     try:
-        with Session(engine) as session:
-            character = TrackerTable(
-                name=name,
-                init=0,
-                player=False,
-                user=user,
-                guild=server.id
-            )
-            session.add(character)
-            session.commit()
-            return True
+        emp = db.Table(tablename, metadata,
+                       db.Column('id', db.INTEGER(), autoincrement=True, primary_key=True),
+                       db.Column('name', db.String(255), nullable=False, unique=True),
+                       db.Column('init', db.INTEGER(), default=0),
+                       db.Column('player', db.BOOLEAN, default=False),
+                       db.Column('user', db.INTEGER(), nullable=False),
+                       db.Column('current_hp', db.INTEGER(), default=0),
+                       db.Column('max_hp', db.INTEGER(), default=1),
+                       db.Column('temp_hp', db.INTEGER(), default=0),
+                       )
+        stmt = emp.insert().values(
+            name=name,
+            init=0,
+            player=False,
+            user=user,
+            current_hp=HP,
+            max_hp=HP,
+            temp_hp=0
+        )
+        compiled = stmt.compile()
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            conn.commit()
+        return True
     except Exception as e:
+        print(e)
         return False
-
 
 def advance_initiative(server: discord.Guild):
     engine = create_engine(f'sqlite:///{SERVER_DATA}.db', future=True)
     with Session(engine) as session:
         guild = session.execute(select(Global).filter_by(guild_id=server.id)).scalar_one()
 
+
+
+
 def get_init_list(server: discord.Guild):
     engine = create_engine(f'sqlite:///{SERVER_DATA}.db', future=True)
-    with Session(engine) as session:
-        results = session.execute(f"SELECT * FROM Tracker WHERE guild = {server.id} ORDER BY init")
-        for character in results:
-            print(character)
+    tablename = f'Tracker_{server.id}'
+    metadata = db.MetaData()
+    emp = db.Table(tablename, metadata,
+                   db.Column('id', db.INTEGER(), autoincrement=True, primary_key=True),
+                   db.Column('name', db.String(255), nullable=False, unique=True),
+                   db.Column('init', db.INTEGER(), default=0),
+                   db.Column('player', db.BOOLEAN, default=False),
+                   db.Column('user', db.INTEGER(), nullable=False),
+                   db.Column('current_hp', db.INTEGER(), default=0),
+                   db.Column('max_hp', db.INTEGER(), default=1),
+                   db.Column('temp_hp', db.INTEGER(), default=0),
+                   )
+    # TODO - Order this by descending init
+    stmt = emp.select()
+    print(stmt)
+    with engine.connect() as conn:
+        for row in conn.execute(stmt):
+            print(row)
+        return conn.execute(stmt)
+
+    # with Session(engine) as session:
+    #     results = session.execute(f"SELECT * FROM Tracker WHERE guild = {server.id} ORDER BY init")
+    #     for character in results:
+    #         print(character)
 
 
 # The Initiative Cog
@@ -131,8 +225,16 @@ class InitiativeCog(commands.Cog):
             await ctx.respond("Server Failed", ephemeral=True)
 
     @initiative.command(guild_ids=[GUILD])
-    async def add_character(self, ctx: discord.ApplicationContext, name: str):
-        response = add_player(name, ctx.user.id, ctx.guild)
+    async def add_character(self, ctx: discord.ApplicationContext, name: str, hp: int):
+        response = add_player(name, ctx.user.id, ctx.guild, hp)
+        if response:
+            await ctx.respond(f"Character {name} added successfully", ephemeral=True)
+        else:
+            await ctx.respond(f"Error Adding Character", ephemeral=True)
+
+    @initiative.command(guild_ids=[GUILD])
+    async def add_npc(self, ctx: discord.ApplicationContext, name: str, hp: int):
+        response = add_npc(name, ctx.user.id, ctx.guild, hp)
         if response:
             await ctx.respond(f"Character {name} added successfully", ephemeral=True)
         else:
@@ -146,8 +248,6 @@ class InitiativeCog(commands.Cog):
             guild.initiative = 100
             session.commit()
         get_init_list(ctx.guild)
-
-
 
 
 
