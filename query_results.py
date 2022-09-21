3b704b6987964cde297a166e52e54b8b64e0931e
Artica2012 <schneiaj@gmail.com>
2022-09-20 20:55:54 -0700

    Error Reporting Code complete for query_results.py

--- query_results.py
+++ query_results.py
@@ -14,6 +14,7 @@ import os
 from dotenv import load_dotenv
 
 from ui_components import QuerySelectButton
+from error_handling_reporting import ErrorReport
 
 
 load_dotenv(verbose=True)
@@ -54,74 +55,85 @@ class QueryCog(commands.Cog):
         # attach to the engine and query the proper table
         engine = get_db_engine(user=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT, db=DATABASE)
         metadata = db.MetaData()
-        emp = power_table(metadata)
-        stmt = emp.select().where(emp.c.Title.ilike(f'%{query}%'))
-        compile = stmt.compile()
-        with engine.connect() as conn:
-            results = []
-            for row in conn.execute(stmt):
-                results.append(row)
+        try:
+            emp = power_table(metadata)
+            stmt = emp.select().where(emp.c.Title.ilike(f'%{query}%'))
+            compile = stmt.compile()
+            with engine.connect() as conn:
+                results = []
+                for row in conn.execute(stmt):
+                    results.append(row)
 
-        # Create the view
-        view = discord.ui.View(timeout=None)  # Keep it persistent
-        if not results:
-            await ctx.respond("No Results Found")
-            return
-        # Add a button to the view for each of the first 10 results
-        for result in results[0:10]:
-            name = f"{result[2]}, Level:{result[3]}"
-            link = result[8]
-            view.add_item((QuerySelectButton(name, f"{name}{ctx.user}", link=link)))
-        await ctx.send_followup(f"Query Results: Powers - {query}", view=view)
+            # Create the view
+            view = discord.ui.View(timeout=None)  # Keep it persistent
+            if not results:
+                await ctx.respond("No Results Found")
+                return
+            # Add a button to the view for each of the first 10 results
+            for result in results[0:10]:
+                name = f"{result[2]}, Level:{result[3]}"
+                link = result[8]
+                view.add_item((QuerySelectButton(name, f"{name}{ctx.user}", link=link)))
+            await ctx.send_followup(f"Query Results: Powers - {query}", view=view)
+        except Exception as e:
+            report = ErrorReport(ctx, "power query", e, self.bot)
+            await report.report()
 
     @query.command(description="Power Query")
     async def disease(self, ctx: discord.ApplicationContext, query: str):
         await ctx.response.defer()
         engine = get_db_engine(user=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT, db=DATABASE)
         metadata = db.MetaData()
-        emp = disease_table(metadata)
-        stmt = emp.select().where(emp.c.Title.ilike(f'%{query}%'))
-        compile = stmt.compile()
-        print(compile)
-        with engine.connect() as conn:
-            results = []
-            for row in conn.execute(stmt):
-                results.append(row)
+        try:
+            emp = disease_table(metadata)
+            stmt = emp.select().where(emp.c.Title.ilike(f'%{query}%'))
+            compile = stmt.compile()
+            print(compile)
+            with engine.connect() as conn:
+                results = []
+                for row in conn.execute(stmt):
+                    results.append(row)
 
-        view = discord.ui.View(timeout=None)  # Keep it persistent
-        if not results:
-            await ctx.respond("No Results Found")
-            return
-        for result in results[0:10]:
-            name = f"{result[2]}, Level: {result[3]}"
-            link = result[6]
-            view.add_item((QuerySelectButton(name, f"{name}{ctx.user}", link=link)))
-        await ctx.send_followup(f"Query Results: Powers - {query}", view=view)
+            view = discord.ui.View(timeout=None)  # Keep it persistent
+            if not results:
+                await ctx.respond("No Results Found")
+                return
+            for result in results[0:10]:
+                name = f"{result[2]}, Level: {result[3]}"
+                link = result[6]
+                view.add_item((QuerySelectButton(name, f"{name}{ctx.user}", link=link)))
+            await ctx.send_followup(f"Query Results: Powers - {query}", view=view)
+        except Exception as e:
+            report = ErrorReport(ctx, "disease query", e, self.bot)
+            await report.report()
 
     @query.command(description="Feat Query")
     async def feat(self, ctx: discord.ApplicationContext, query: str):
         await ctx.response.defer()
         engine = get_db_engine(user=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT, db=DATABASE)
         metadata = db.MetaData()
-        emp = feat_table(metadata)
-        stmt = emp.select().where(emp.c.Title.ilike(f'%{query}%'))
-        compile = stmt.compile()
-        with engine.connect() as conn:
-            results = []
-            # print(conn.execute(stmt))
-            for row in conn.execute(stmt):
-                results.append(row)
+        try:
+            emp = feat_table(metadata)
+            stmt = emp.select().where(emp.c.Title.ilike(f'%{query}%'))
+            compile = stmt.compile()
+            with engine.connect() as conn:
+                results = []
+                # print(conn.execute(stmt))
+                for row in conn.execute(stmt):
+                    results.append(row)
 
-        view = discord.ui.View(timeout=None)  # Keep it persistent
-        if not results:
-            await ctx.respond("No Results Found")
-            return
-        for result in results[0:10]:
-            name = f"{result[2]}"
-            link = result[6]
-            view.add_item((QuerySelectButton(name, f"{name}{ctx.user}", link=link)))
-        await ctx.send_followup(f"Query Results: Powers - {query}", view=view)
-
+            view = discord.ui.View(timeout=None)  # Keep it persistent
+            if not results:
+                await ctx.respond("No Results Found")
+                return
+            for result in results[0:10]:
+                name = f"{result[2]}"
+                link = result[6]
+                view.add_item((QuerySelectButton(name, f"{name}{ctx.user}", link=link)))
+            await ctx.send_followup(f"Query Results: Powers - {query}", view=view)
+        except Exception as e:
+            report = ErrorReport(ctx, "feat query", e, self.bot)
+            await report.report()
 
 def setup(bot):
     bot.add_cog(QueryCog(bot))
