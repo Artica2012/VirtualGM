d2e71c2b3a332ddbaaf3d0830e75259c2c81d031
Artica2012 <schneiaj@gmail.com>
2022-10-23 20:59:05 -0700

    Start of the Pathfinder 2e specific code branch

---
+++ PF2e/pf2_cog.py
@@ -0,0 +1,55 @@
+# pf2_cog.py
+# For slash commands specific to oathfinder 2e
+# system specific module
+
+import os
+
+# imports
+import discord
+import asyncio
+import sqlalchemy as db
+from discord import option
+from discord.commands import SlashCommandGroup, option
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+from sqlalchemy import or_
+from sqlalchemy import select, update, delete
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import Session, selectinload, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database_models import Global, Base, TrackerTable, ConditionTable, MacroTable, get_tracker_table, \
+    get_condition_table, get_macro_table
+from database_operations import get_asyncio_db_engine
+from dice_roller import DiceRoller
+from error_handling_reporting import ErrorReport
+from time_keeping_functions import output_datetime, check_timekeeper, advance_time, get_time
+
+# define global variables
+
+load_dotenv(verbose=True)
+if os.environ['PRODUCTION'] == 'True':
+    TOKEN = os.getenv('TOKEN')
+    USERNAME = os.getenv('Username')
+    PASSWORD = os.getenv('Password')
+    HOSTNAME = os.getenv('Hostname')
+    PORT = os.getenv('PGPort')
+else:
+    TOKEN = os.getenv('BETA_TOKEN')
+    USERNAME = os.getenv('BETA_Username')
+    PASSWORD = os.getenv('BETA_Password')
+    HOSTNAME = os.getenv('BETA_Hostname')
+    PORT = os.getenv('BETA_PGPort')
+
+GUILD = os.getenv('GUILD')
+SERVER_DATA = os.getenv('SERVERDATA')
+DATABASE = os.getenv('DATABASE')
+
+class PF2Cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.engine = get_asyncio_db_engine(user=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT, db=SERVER_DATA)
+
+
+def setup(bot):
+    bot.add_cog(PF2Cog(bot))
