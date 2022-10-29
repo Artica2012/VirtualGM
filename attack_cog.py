e1995ebd8a12b8c8e8cdbd300b8c9bbf137c8a46
Artica2012 <schneiaj@gmail.com>
2022-10-29 08:08:11 -0700

    Inteval commit

--- attack_cog.py
+++ attack_cog.py
@@ -133,12 +133,18 @@ class AttackCog(commands.Cog):
             await report.report()
             return False
 
+    # ---------------------------------------------------
+    # ---------------------------------------------------
+    # Slash commands
+
+    attack = SlashCommandGroup("a", "Automatic Attack Commands")
+
+
     @commands.slash_command(name='a', description="Automatic Attack")
     @option('character', description='Character Attacking', autocomplete=character_select_gm)
     @option('target', description="Character to Target", autocomplete=character_select)
     @option('vs', description="Target Attribute", autocomplete=discord.utils.basic_autocomplete(PF2e.pf2_functions.PF2_attributes))
-    @option('save', description="True, Target Save vs Character DC", autocomplete=discord.utils.basic_autocomplete(['True', 'False']))
-    async def attack(self, ctx:discord.ApplicationContext, character:str, target:str, roll:str, vs:str, save:str):
+    async def attack(self, ctx:discord.ApplicationContext, character:str, target:str, roll:str, vs:str):
         metadata = db.MetaData()
         await ctx.response.defer()
         async with self.async_session() as session:
@@ -155,76 +161,145 @@ class AttackCog(commands.Cog):
                 return
             #PF2 specific code
 
-            # Save = False
+            # Dice Roller
+            roller = dice_roller.DiceRoller('')
+            try:
+                dice_result = await roller.attack_roll(roll)
+                total = dice_result[1]
+                dice_string = dice_result[0]
+            except Exception as e:
+                await ctx.send_followup('Error in the dice string. Check Syntax')
 
-            if save == 'True':
-                # TODO - Make this
-                pass
+            try:
+                emp = await get_tracker_table(ctx, metadata, self.engine)
+                con = await get_condition_table(ctx, metadata, self.engine)
+                emp_stmt = emp.select().where(emp.c.name == character)
+                async with self.engine.begin() as conn:
+                    data = []
+                    for row in await conn.execute(emp_stmt):
+                        data.append(row)
+            except NoResultFound as e:
+                await ctx.channel.send(error_not_initialized,
+                                       delete_after=30)
+                return False
+            except Exception as e:
+                print(f'attack: {e}')
+                report = ErrorReport(ctx, "/attack (emp)", e, self.bot)
+                await report.report()
+            try:
+                con_stmt = con.select().where(con.c.character_id == data[0][0]).where(con.c.title == vs)
+                con_data = []
+                async with self.engine.begin() as conn:
+                    for row in await conn.execute(con_stmt):
+                        con_data.append(row)
+                await self.engine.dispose()
+            except NoResultFound as e:
+                await ctx.channel.send(error_not_initialized,
+                                       delete_after=30)
+                return False
+            except Exception as e:
+                print(f'get_cc: {e}')
+                report = ErrorReport(ctx, "/attack (con)", e, self.bot)
+                await report.report()
+
+            goal_value = con_data[0][4]
+            success = False
+            crit = False
+            success_string = ''
+            if total >= goal_value:
+                success = True
+                success_string = "Success"
+                if total >= goal_value+10 or dice_result[1]:
+                    crit = True
+                    success_string = "Critical Success"
             else:
-                # Dice Roller
-                roller = dice_roller.DiceRoller('')
-                try:
-                    dice_result = await roller.attack_roll(roll)
-                    total = dice_result[1]
-                    dice_string = dice_result[0]
-                except Exception as e:
-                    await ctx.send_followup('Error in the dice string. Check Syntax')
-
-                try:
-                    emp = await get_tracker_table(ctx, metadata, self.engine)
-                    con = await get_condition_table(ctx, metadata, self.engine)
-                    emp_stmt = emp.select().where(emp.c.name == character)
-                    async with self.engine.begin() as conn:
-                        data = []
-                        for row in await conn.execute(emp_stmt):
-                            data.append(row)
-                except NoResultFound as e:
-                    await ctx.channel.send(error_not_initialized,
-                                           delete_after=30)
-                    return False
-                except Exception as e:
-                    print(f'attack: {e}')
-                    report = ErrorReport(ctx, "/attack (emp)", e, self.bot)
-                    await report.report()
-                try:
-                    con_stmt = con.select().where(con.c.character_id == data[0][0]).where(con.c.title == vs)
-                    con_data = []
-                    async with self.engine.begin() as conn:
-                        for row in await conn.execute(con_stmt):
-                            con_data.append(row)
-                    await self.engine.dispose()
-                except NoResultFound as e:
-                    await ctx.channel.send(error_not_initialized,
-                                           delete_after=30)
-                    return False
-                except Exception as e:
-                    print(f'get_cc: {e}')
-                    report = ErrorReport(ctx, "/attack (con)", e, self.bot)
-                    await report.report()
-
-                goal_value = con_data[0][4]
                 success = False
-                crit = False
-                success_string = ''
-                if total >= goal_value:
-                    success = True
-                    success_string = "Success"
-                    if total >= goal_value+10 or dice_result[1]:
-                        crit = True
-                        success_string = "Critical Success"
-                else:
-                    success = False
-                    success_string = "Failure"
-                    if total <= goal_value-10 or dice_result[2]:
-                        crit = True
-                        success_string = "Critical Failure"
+                success_string = "Failure"
+                if total <= goal_value-10 or dice_result[2]:
+                    crit = True
+                    success_string = "Critical Failure"
 
-                # Format output string
-                output_string = f"{character} vs {target} {vs}:\n" \
-                                f"{dice_string} = {total}\n" \
-                                f"{success_string}"
-                await ctx.send_followup(output_string)
+            # Format output string
+            output_string = f"{character} vs {target} {vs}:\n" \
+                            f"{dice_string} = {total}\n" \
+                            f"{success_string}"
+            await ctx.send_followup(output_string)
 
+
+    @attack.command(description="Saving Throw")
+    @option('character', description='Character Attacking', autocomplete=character_select_gm)
+    @option('target', description="Character to Target", autocomplete=character_select)
+    @option('vs', description="Target Attribute", autocomplete=discord.utils.basic_autocomplete(PF2e.pf2_functions.PF2_attributes))
+    async def attack(self, ctx:discord.ApplicationContext, character:str, target:str, vs:str):
+        metadata = db.MetaData()
+        await ctx.response.defer()
+        async with self.async_session() as session:
+            result = await session.execute(select(Global).where(
+                or_(
+                    Global.tracker_channel == ctx.interaction.channel_id,
+                    Global.gm_tracker_channel == ctx.interaction.channel_id
+                )
+            )
+            )
+            guild = result.scalars().one()
+            if not guild.system:
+                await ctx.respond("No system set, command inactive.")
+                return
+            #PF2 specific code
+            try:
+                emp = await get_tracker_table(ctx, metadata, self.engine)
+                con = await get_condition_table(ctx, metadata, self.engine)
+
+
+                char_sel_stmt = emp.select().where(emp.c.name == character)
+                target_sel_stmt = emp.select().where(emp.c.name == target)
+                async with self.engine.begin() as conn:
+                    data = []
+                    for row in await conn.execute(char_sel_stmt):
+                        data.append(row)
+
+
+
+
+            except NoResultFound as e:
+                await ctx.channel.send(error_not_initialized,
+                                       delete_after=30)
+                return False
+            except Exception as e:
+                print(f'attack: {e}')
+                report = ErrorReport(ctx, "/attack (emp)", e, self.bot)
+                await report.report()
+
+
+
+
+
+            try:
+                con_stmt = con.select().where(con.c.character_id == data[0][0]).where(con.c.title == vs)
+                con_data = []
+                async with self.engine.begin() as conn:
+                    for row in await conn.execute(con_stmt):
+                        con_data.append(row)
+                await self.engine.dispose()
+            except NoResultFound as e:
+                await ctx.channel.send(error_not_initialized,
+                                       delete_after=30)
+                return False
+            except Exception as e:
+                print(f'get_cc: {e}')
+                report = ErrorReport(ctx, "/attack (con)", e, self.bot)
+                await report.report()
+
+
+
+
+            roller = dice_roller.DiceRoller('')
+            try:
+                dice_result = await roller.attack_roll("1d20")
+                total = dice_result[1]
+                dice_string = dice_result[0]
+            except Exception as e:
+                await ctx.send_followup('Error in the dice string. Check Syntax')
 
 
 
