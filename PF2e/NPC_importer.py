60b0d773eb6dad5ab24cefff4889392580f6e9e2
Artica2012 <schneiaj@gmail.com>
2023-02-02 20:30:16 -0800

    Fixed a breaking bug in the elite/weak to the npc importer

--- PF2e/NPC_importer.py
+++ PF2e/NPC_importer.py
@@ -50,7 +50,7 @@ SERVER_DATA = os.getenv('SERVERDATA')
 DATABASE = os.getenv('DATABASE')
 
 
-async def npc_lookup(ctx: discord.ApplicationContext, engine, lookup_engine, bot, name: str, lookup: str, elite:str):
+async def npc_lookup(ctx: discord.ApplicationContext, engine, lookup_engine, bot, name: str, lookup: str, elite: str):
     async_session = sessionmaker(lookup_engine, expire_on_commit=False, class_=AsyncSession)
     async with async_session() as session:
         result = await session.execute(select(NPC)
@@ -70,7 +70,7 @@ async def npc_lookup(ctx: discord.ApplicationContext, engine, lookup_engine, bot
 
 
 class PF2NpcSelectButton(discord.ui.Button):
-    def __init__(self, ctx: discord.ApplicationContext, engine, bot: discord.Bot, data, name, elite:str):
+    def __init__(self, ctx: discord.ApplicationContext, engine, bot: discord.Bot, data, name, elite: str):
         self.ctx = ctx
         self.engine = engine
         self.bot = bot
@@ -88,15 +88,15 @@ class PF2NpcSelectButton(discord.ui.Button):
         message = interaction.message
         await message.delete()
 
-        #elite/weak adjustments
+        # elite/weak adjustments
         hp_mod = 0
-        stat_mod =0
+        stat_mod = 0
         if self.elite == 'elite':
             if self.data.level <= 1:
                 hp_mod = 10
-            elif self.data.level <=4:
+            elif self.data.level <= 4:
                 hp_mod = 15
-            elif self.data.level <=19:
+            elif self.data.level <= 19:
                 hp_mod = 20
             else:
                 hp_mod = 30
@@ -104,15 +104,13 @@ class PF2NpcSelectButton(discord.ui.Button):
         if self.elite == 'weak':
             if self.data.level <= 1:
                 hp_mod = -10
-            elif self.data.level <=4:
+            elif self.data.level <= 4:
                 hp_mod = -15
-            elif self.data.level <=19:
+            elif self.data.level <= 19:
                 hp_mod = -20
             else:
                 hp_mod = -30
             stat_mod = -2
-
-
 
         # try:
         dice = DiceRoller('')
@@ -132,11 +130,14 @@ class PF2NpcSelectButton(discord.ui.Button):
             if guild.initiative != None:
                 try:
                     # print(f"Init: {init}")
-                    initiative_num = int(self.data.init)+stat_mod
+                    initiative_num = int(self.data.init) + stat_mod
                     print(initiative_num)
                 except:
                     try:
-                        roll = await dice.plain_roll(f"{self.data.init}+{stat_mod}")
+                        if self.elite == 'weak':
+                            roll = await dice.plain_roll(f"{self.data.init}{stat_mod}")
+                        else:
+                            roll = await dice.plain_roll(f"{self.data.init}+{stat_mod}")
                         initiative_num = roll[1]
                         print(initiative_num)
                         if type(initiative_num) != int:
@@ -153,8 +154,8 @@ class PF2NpcSelectButton(discord.ui.Button):
                     init=initiative_num,
                     player=False,
                     user=self.ctx.user.id,
-                    current_hp=self.data.hp+hp_mod,
-                    max_hp=self.data.hp+hp_mod,
+                    current_hp=self.data.hp + hp_mod,
+                    max_hp=self.data.hp + hp_mod,
                     temp_hp=0
                 )
                 session.add(tracker)
@@ -169,34 +170,34 @@ class PF2NpcSelectButton(discord.ui.Button):
             session.add(Condition(
                 character_id=character.id,
                 title='AC',
-                number=self.data.ac+stat_mod,
+                number=self.data.ac + stat_mod,
                 counter=True,
                 visible=False))
             session.add(Condition(
                 character_id=character.id,
                 title='Fort',
-                number=self.data.fort+stat_mod,
+                number=self.data.fort + stat_mod,
                 counter=True,
                 visible=False
             ))
             session.add(Condition(
                 character_id=character.id,
                 title='Reflex',
-                number=self.data.reflex+stat_mod,
+                number=self.data.reflex + stat_mod,
                 counter=True,
                 visible=False
             ))
             session.add(Condition(
                 character_id=character.id,
                 title='Will',
-                number=self.data.will+stat_mod,
+                number=self.data.will + stat_mod,
                 counter=True,
                 visible=False
             ))
             session.add(Condition(
                 character_id=character.id,
                 title='DC',
-                number=self.data.dc+stat_mod,
+                number=self.data.dc + stat_mod,
                 counter=True,
                 visible=False
             ))
@@ -215,18 +216,33 @@ class PF2NpcSelectButton(discord.ui.Button):
                 base_name = split_string[0].strip()
                 attack_string = split_string[1].strip()
                 damage_string = split_string[2].strip()
-                attack_macro = Macro(
-                    character_id=character.id,
-                    name=f"{base_name} - Attack",
-                    macro=f"{attack_string}+{stat_mod}"
-                )
+                if self.elite == 'weak':
+                    attack_macro = Macro(
+                        character_id=character.id,
+                        name=f"{base_name} - Attack",
+                        macro=f"{attack_string}{stat_mod}"
+                    )
+                else:
+                    attack_macro = Macro(
+                        character_id=character.id,
+                        name=f"{base_name} - Attack",
+                        macro=f"{attack_string}+{stat_mod}"
+                    )
                 session.add(attack_macro)
                 print('Attack Added')
-                damage_macro = Macro(
-                    character_id=character.id,
-                    name=f"{base_name} - Damage",
-                    macro=f"{damage_string}+{stat_mod}"
-                )
+                if self.elite == 'weak':
+                    damage_macro = Macro(
+                        character_id=character.id,
+                        name=f"{base_name} - Damage",
+                        macro=f"{damage_string}{stat_mod}"
+                    )
+                else:
+                    damage_macro = Macro(
+                        character_id=character.id,
+                        name=f"{base_name} - Damage",
+                        macro=f"{damage_string}+{stat_mod}"
+                    )
+
                 session.add(damage_macro)
                 print("Damage Added")
             await session.commit()
