f7fe38996fae7539bedecbb155421875d37634ce
Artica2012 <schneiaj@gmail.com>
2023-04-06 00:09:23 -0700

    - Updated error handling to the initiative cog

--- initiative.py
+++ initiative.py
@@ -57,15 +57,18 @@ class InitiativeCog(commands.Cog):
     @tasks.loop(minutes=1)
     async def update_status(self):
         engine = get_asyncio_db_engine(user=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT, db=SERVER_DATA)
-        async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-        async with async_session() as session:
-            guild = await session.execute(select(Global))
-            result = guild.scalars().all()
-            count = len(result)
-        async with self.lock:
-            await self.bot.change_presence(
-                activity=discord.Game(name=f"ttRPGs in {count} tables across the digital universe.")
-            )
+        try:
+            async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+            async with async_session() as session:
+                guild = await session.execute(select(Global))
+                result = guild.scalars().all()
+                count = len(result)
+            async with self.lock:
+                await self.bot.change_presence(
+                    activity=discord.Game(name=f"ttRPGs in {count} tables across the digital universe.")
+                )
+        except Exception as e:
+            logging.error(f"Initiative Cog = Update Status: {e}")
         await engine.dispose()
 
     # Don't start the loop unti the bot is ready
@@ -107,8 +110,14 @@ class InitiativeCog(commands.Cog):
         elif player == "npc":
             player_bool = False
 
-        Utilities = await get_utilities(ctx, engine=engine)
-        response = await Utilities.add_character(self.bot, name, hp, player_bool, initiative)
+        try:
+            Utilities = await get_utilities(ctx, engine=engine)
+            response = await Utilities.add_character(self.bot, name, hp, player_bool, initiative)
+        except Exception as e:
+            logging.warning(f"char add {e}")
+            report = ErrorReport(ctx, "/char add", e, self.bot)
+            await report.report()
+
         if response:
             await ctx.respond(f"Character {name} added successfully.", ephemeral=True)
             Tracker_Model = await get_tracker_model(ctx, self.bot, engine=engine)
@@ -139,9 +148,16 @@ class InitiativeCog(commands.Cog):
         engine = get_asyncio_db_engine(user=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT, db=SERVER_DATA)
         guild = await get_guild(ctx, None)
         response = False
+
         if await auto_complete.hard_lock(ctx, name):
-            Character_Model = await get_character(name, ctx, guild=guild, engine=engine)
-            response = await Character_Model.edit_character(name, hp, initiative, active, player, self.bot)
+            try:
+                Character_Model = await get_character(name, ctx, guild=guild, engine=engine)
+                response = await Character_Model.edit_character(name, hp, initiative, active, player, self.bot)
+            except Exception as e:
+                logging.warning(f"char edit {e}")
+                report = ErrorReport(ctx, "/char edit", e, self.bot)
+                await report.report()
+
             if not response:
                 await ctx.respond("Error Editing Character", ephemeral=True)
             else:
@@ -162,15 +178,22 @@ class InitiativeCog(commands.Cog):
     async def copy(self, ctx: discord.ApplicationContext, name: str, new_name: str):
         engine = get_asyncio_db_engine(user=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT, db=SERVER_DATA)
         await ctx.response.defer(ephemeral=True)
-        Utilites = await get_utilities(ctx, engine=engine)
         response = False
-        response = await Utilites.copy_character(name, new_name)
+
+        try:
+            Utilites = await get_utilities(ctx, engine=engine)
+            response = await Utilites.copy_character(name, new_name)
+        except Exception as e:
+            logging.warning(f"char copy {e}")
+            report = ErrorReport(ctx, "/char copy", e, self.bot)
+            await report.report()
         if response:
             await ctx.send_followup(f"{new_name} Created", ephemeral=True)
+            Tracker_Model = await get_tracker_model(ctx, self.bot, engine=engine)
+            await Tracker_Model.update_pinned_tracker()
         else:
             await ctx.send_followup("Error Copying Character", ephemeral=True)
-        Tracker_Model = await get_tracker_model(ctx, self.bot, engine=engine)
-        await Tracker_Model.update_pinned_tracker()
+
         await engine.dispose()
 
     @char.command(description="Delete NPC")
@@ -193,8 +216,14 @@ class InitiativeCog(commands.Cog):
                         ephemeral=True,
                     )
                 else:
-                    Utilities = await get_utilities(ctx, guild=guild, engine=engine)
-                    result = await Utilities.delete_character(name)
+                    result = False
+                    try:
+                        Utilities = await get_utilities(ctx, guild=guild, engine=engine)
+                        result = await Utilities.delete_character(name)
+                    except Exception as e:
+                        logging.warning(f"char delete {e}")
+                        report = ErrorReport(ctx, "/char delete", e, self.bot)
+                        await report.report()
                     if result:
                         await ctx.send_followup(f"{name} deleted", ephemeral=True)
                         Tracker_Model = await get_tracker_model(ctx, self.bot, guild=guild, engine=engine)
@@ -223,9 +252,15 @@ class InitiativeCog(commands.Cog):
         await ctx.response.defer(ephemeral=True)
         engine = get_asyncio_db_engine(user=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT, db=SERVER_DATA)
         if await auto_complete.hard_lock(ctx, name):
-            Character_Model = await get_character(name, ctx, engine=engine)
-            embed = await Character_Model.get_char_sheet(self.bot)
-            await ctx.send_followup(embeds=embed)
+            try:
+                Character_Model = await get_character(name, ctx, engine=engine)
+                embed = await Character_Model.get_char_sheet(self.bot)
+                await ctx.send_followup(embeds=embed)
+            except Exception as e:
+                logging.warning(f"char sheet {e}")
+                report = ErrorReport(ctx, "/char sheet", e, self.bot)
+                await report.report()
+                await ctx.send_followup("Error displaying character sheet. Ensure valid character.")
         else:
             ctx.send_followup("You do not have the appropriate permissions to view this character.")
         await engine.dispose()
@@ -281,13 +316,16 @@ class InitiativeCog(commands.Cog):
             return False
         except IndexError:
             await ctx.respond("Ensure that you have added characters to the initiative list.")
-        except Exception:
+        except Exception as e:
             await ctx.respond("Failed")
+            logging.warning(f"/i mange {e}")
+            report = ErrorReport(ctx, "/i manage", e, self.bot)
+            await report.report()
+
         await engine.dispose()
 
     @i.command(
         description="Advance Initiative",
-        # guild_ids=[GUILD]
     )
     async def next(self, ctx: discord.ApplicationContext):
         engine = get_asyncio_db_engine(user=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT, db=SERVER_DATA)
@@ -309,7 +347,6 @@ class InitiativeCog(commands.Cog):
 
     @i.command(
         description="Set Init (Number or XdY+Z)",
-        # guild_ids=[GUILD]
     )
     @option(
         "character",
@@ -330,14 +367,14 @@ class InitiativeCog(commands.Cog):
             )
         else:
             try:
-                # roll = d20.roll(initiative)
                 model = await get_character(character, ctx, guild=guild, engine=engine)
                 output = await model.set_init(initiative)
                 await ctx.respond(output)
+                Tracker_Object = await get_tracker_model(ctx, self.bot, engine=engine)
+                await Tracker_Object.update_pinned_tracker()
             except Exception as e:
                 await ctx.respond(f"Failed to set initiative for {character}.\n{e}", ephemeral=True)
-        Tracker_Object = await get_tracker_model(ctx, self.bot, engine=engine)
-        await Tracker_Object.update_pinned_tracker()
+
         await engine.dispose()
 
     @i.command(
@@ -351,29 +388,34 @@ class InitiativeCog(commands.Cog):
         response = False
         await ctx.response.defer()
         guild = await get_guild(ctx, None)
-
-        model = await get_character(name, ctx, guild=guild, engine=engine)
-        if mode == "Temporary HP":
-            response = await model.add_thp(amount)
-            if response:
-                await ctx.respond(f"{amount} Temporary HP added to {name}.")
-        else:
-            if mode == "Heal":
-                heal = True
+        try:
+            model = await get_character(name, ctx, guild=guild, engine=engine)
+            if mode == "Temporary HP":
+                response = await model.add_thp(amount)
+                if response:
+                    await ctx.respond(f"{amount} Temporary HP added to {name}.")
             else:
-                heal = False
-            response = await model.change_hp(amount, heal)
+                if mode == "Heal":
+                    heal = True
+                else:
+                    heal = False
+                response = await model.change_hp(amount, heal)
+        except Exception as e:
+            await ctx.respond("Error", ephemeral=True)
+            logging.warning(f"/i hp: {e}")
+            report = ErrorReport(ctx, "slash command /i hp", e, self.bot)
+            await report.report()
 
         if not response:
             await ctx.respond("Failed", ephemeral=True)
+        else:
+            Tracker_Object = await get_tracker_model(ctx, self.bot, engine=engine, guild=guild)
+            await Tracker_Object.update_pinned_tracker()
 
-        Tracker_Object = await get_tracker_model(ctx, self.bot, engine=engine, guild=guild)
-        await Tracker_Object.update_pinned_tracker()
         await engine.dispose()
 
     @cc.command(
         description="Add conditions and counters",
-        # guild_ids=[GUILD]
     )
     @option("character", description="Character to select", autocomplete=character_select)
     @option("title", autocomplete=add_condition_select)
@@ -409,21 +451,27 @@ class InitiativeCog(commands.Cog):
             auto_bool = True
         else:
             auto_bool = False
+        response = False
 
-        model = await get_character(character, ctx, guild=guild, engine=engine)
-        Tracker = await get_tracker_model(ctx, self.bot, guild=guild, engine=engine)
-        response = await model.set_cc(title, counter_bool, number, unit, auto_bool, flex=flex_bool, data=data)
-        await Tracker.update_pinned_tracker()
+        try:
+            model = await get_character(character, ctx, guild=guild, engine=engine)
+            response = await model.set_cc(title, counter_bool, number, unit, auto_bool, flex=flex_bool, data=data)
+        except Exception as e:
+            await ctx.respond("Error", ephemeral=True)
+            logging.warning(f"/cc new: {e}")
+            report = ErrorReport(ctx, "slash command /cc new", e, self.bot)
+            await report.report()
 
         if response:
             await ctx.send_followup(f"Condition {title} added on {character}")
+            Tracker_Object = await get_tracker_model(ctx, self.bot, engine=engine, guild=guild)
+            await Tracker_Object.update_pinned_tracker()
         else:
             await ctx.send_followup("Add Condition/Counter Failed")
         await engine.dispose()
 
     @cc.command(
         description="Edit or remove conditions and counters",
-        # guild_ids=[GUILD]
     )
     @option("mode", choices=["edit", "delete"])
     @option("character", description="Character to select", autocomplete=character_select)
@@ -435,30 +483,36 @@ class InitiativeCog(commands.Cog):
         engine = get_asyncio_db_engine(user=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT, db=SERVER_DATA)
         await ctx.response.defer(ephemeral=True)
         Character_Model = await get_character(character, ctx, engine=engine)
-
-        if mode == "delete":
-            result = await Character_Model.delete_cc(condition)
-            if result:
-                await ctx.send_followup("Successful Delete", ephemeral=True)
-                await ctx.send(f"{condition} on {character} deleted.")
-        elif mode == "edit":
-            # print("editing")
-            if value is not None:
-                result = await Character_Model.edit_cc(condition, value)
+        try:
+            if mode == "delete":
+                result = await Character_Model.delete_cc(condition)
                 if result:
-                    await ctx.send_followup(f"{condition} on {character} updated to {value}.")
+                    await ctx.send_followup("Successful Delete", ephemeral=True)
+                    await ctx.send(f"{condition} on {character} deleted.")
+            elif mode == "edit":
+                # print("editing")
+                if value is not None:
+                    result = await Character_Model.edit_cc(condition, value)
+                    if result:
+                        await ctx.send_followup(f"{condition} on {character} updated to {value}.")
+                    else:
+                        await ctx.send_followup("Error")
                 else:
-                    await ctx.send_followup("Error")
+                    output = await edit_cc_interface(ctx, engine, character, condition, self.bot)
+                    if output[0] is not None:
+                        await ctx.send_followup(output[0], view=output[1], ephemeral=True)
+                    else:
+                        await ctx.send_followup("Error")
             else:
-                output = await edit_cc_interface(ctx, engine, character, condition, self.bot)
-                if output[0] is not None:
-                    await ctx.send_followup(output[0], view=output[1], ephemeral=True)
-                else:
-                    await ctx.send_followup("Error")
-        else:
-            await ctx.send_followup("Invalid Input", ephemeral=True)
-        Tracker_Model = await get_tracker_model(ctx, self.bot, engine=engine)
-        await Tracker_Model.update_pinned_tracker()
+                await ctx.send_followup("Invalid Input", ephemeral=True)
+
+            Tracker_Model = await get_tracker_model(ctx, self.bot, engine=engine)
+            await Tracker_Model.update_pinned_tracker()
+        except Exception as e:
+            await ctx.send_followup("Error", ephemeral=True)
+            logging.warning(f"/cc modify: {e}")
+            report = ErrorReport(ctx, "slash command /cc modify", e, self.bot)
+            await report.report()
         await engine.dispose()
 
 
