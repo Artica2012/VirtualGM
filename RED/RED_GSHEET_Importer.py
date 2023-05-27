2b32d4ece8ec74c20cc49b85a3a8f9dfcad26cea
Artica2012 <schneiaj@gmail.com>
2023-05-26 23:32:59 -0700

    RED  - Added the RED cog  - Began work on the RED g-sheet character importer

--- RED/RED_GSHEET_Importer.py
+++ RED/RED_GSHEET_Importer.py
@@ -5,6 +5,7 @@ import d20
 import discord
 import numpy
 import pandas as pd
+from pandas._libs.internals import defaultdict
 from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
@@ -17,21 +18,6 @@ from database_operations import USERNAME, PASSWORD, HOSTNAME, PORT, SERVER_DATA,
 from EPF.EPF_NPC_Importer import write_resitances
 
 Interpreter = {
-    "U": 0,
-    "T": 2,
-    "E": 4,
-    "M": 6,
-    "L": 8,
-    "Str": "str",
-    "Dex": "dex",
-    "Con": "con",
-    "Int": "itl",
-    "Wis": "wis",
-    "Cha": "cha",
-    "None": "",
-    "Striking": "striking",
-    "Greater Striking": "greaterStriking",
-    "Major Striking": "majorStriking",
     "Yes": True,
     "No": False,
     numpy.nan: "",
@@ -65,33 +51,26 @@ async def red_g_sheet_import(
         headers = list(df.columns.values)
         # print(headers)
         # print(headers[0])
-        # print(df)
+        print(df)
 
         decision_header = headers[0].strip()
 
         if decision_header == "CyberpunkRED:":
-            (character,) = await red_g_sheet_character_import(ctx, char_name, df, engine, guild)
+            character = await red_g_sheet_character_import(ctx, char_name, df, engine, guild)
         else:
             return False
+
+        init_string = f"1d10+{character['stats']['ref']['value']}"
 
         initiative_num = 0
         if not overwrite:
             if guild.initiative is not None:
                 # print("In initiative")
                 try:
-                    perception_mod = character["level"] + character["perception"] + floor((character["wis"] - 10) / 2)
-                    roll = d20.roll(f"1d20+{perception_mod}")
+                    roll = d20.roll(init_string)
                     initiative_num = roll.total
                 except Exception:
                     initiative_num = 0
-
-        if "lore" in character.keys():
-            print("lore")
-            lore = character["lore"]
-            print(lore)
-        else:
-            print("not lore")
-            lore = ""
 
         if overwrite:
             async with async_session() as session:
@@ -101,57 +80,17 @@ async def red_g_sheet_import(
                 character_data = query.scalars().one()
 
                 character_data.max_hp = character["hp"]
-                character_data.char_class = character["class"]
-                character_data.ac_base = character["ac_base"]
+                character_data.char_class = character["role"]
+                character_data.init_string = init_string
                 character_data.level = character["level"]
-                character_data.class_dc = character["class_dc"]
-                character_data.str = character["str"]
-                character_data.dex = character["dex"]
-                character_data.con = character["con"]
-                character_data.itl = character["itl"]
-                character_data.wis = character["wis"]
-                character_data.cha = character["cha"]
-                character_data.fort_pof = character["fort"]
-                character_data.reflex_prof = character["reflex"]
-                character_data.will_prof = character["will"]
-                character_data.perception_prof = character["perception"]
-                character_data.class_prof = character["class_prof"]
-                character_data.key_ability = character["key_ability"]
-                character_data.unarmored_prof = character["unarmored"]
-                character_data.light_armor_prof = character["light"]
-                character_data.medium_armor_prof = character["medium"]
-                character_data.heavy_armor_prof = character["heavy"]
+                character_data.humanity = character["humanity"]
+                character_data.stats = character["stats"]
+                character_data.skills = character["skills"]
+                character_data.attacks = character["attacks"]
+                character_data.armor = character["armor"]
+                character_data.cyber = character["cyber"]
+                character_data.net = character["net"]
 
-                character_data.unarmed_prof = character["unarmed"]
-                character_data.simple_prof = character["simple"]
-                character_data.martial_prof = character["martial"]
-                character_data.advanced_prof = character["advanced"]
-
-                character_data.arcane_prof = character["arcane"]
-                character_data.divine_prof = character["divine"]
-                character_data.occult_prof = character["occult"]
-                character_data.primal_prof = character["primal"]
-
-                character_data.acrobatics_prof = character["acrobatics"]
-                character_data.arcana_prof = character["arcana"]
-                character_data.athletics_prof = character["athletics"]
-                character_data.crafting_prof = character["crafting"]
-                character_data.deception_prof = character["deception"]
-                character_data.diplomacy_prof = character["diplomacy"]
-                character_data.intimidation_prof = character["intimidation"]
-                character_data.medicine_prof = character["medicine"]
-                character_data.nature_prof = character["nature"]
-                character_data.occultism_prof = character["occultism"]
-                character_data.performance_prof = character["performance"]
-                character_data.religion_prof = character["religion"]
-                character_data.society_prof = character["society"]
-                character_data.stealth_prof = character["stealth"]
-                character_data.survival_prof = character["survival"]
-                character_data.thievery_prof = character["thievery"]
-                character_data.feats = character["feats"]
-                # character_data.spells = spells
-                # character_data.attacks = attacks
-                character_data.lores = lore
                 if image is not None:
                     character.pic = image
 
@@ -162,64 +101,27 @@ async def red_g_sheet_import(
                 async with session.begin():
                     new_char = red_tracker(
                         name=char_name,
+                        init=initiative_num,
                         player=True if "npc" not in character.keys() else False,
-                        active=character["active"],
                         user=ctx.user.id,
                         current_hp=character["hp"],
                         max_hp=character["hp"],
                         temp_hp=0,
-                        char_class=character["class"],
+                        init_string=init_string,
+                        active=character["active"],
+                        char_class=character["role"],
                         level=character["level"],
-                        ac_base=character["ac_base"],
-                        init=initiative_num,
-                        class_prof=character["class_prof"],
-                        class_dc=character["class_dc"],
-                        str=character["str"],
-                        dex=character["dex"],
-                        con=character["con"],
-                        itl=character["itl"],
-                        wis=character["wis"],
-                        cha=character["cha"],
-                        fort_prof=character["fort"],
-                        reflex_prof=character["reflex"],
-                        will_prof=character["will"],
-                        unarmored_prof=character["unarmored"],
-                        light_armor_prof=character["light"],
-                        medium_armor_prof=character["medium"],
-                        heavy_armor_prof=character["heavy"],
-                        unarmed_prof=character["unarmed"],
-                        simple_prof=character["simple"],
-                        martial_prof=character["martial"],
-                        advanced_prof=character["advanced"],
-                        arcane_prof=character["arcane"],
-                        divine_prof=character["divine"],
-                        occult_prof=character["occult"],
-                        primal_prof=character["primal"],
-                        acrobatics_prof=character["acrobatics"],
-                        arcana_prof=character["arcana"],
-                        athletics_prof=character["athletics"],
-                        crafting_prof=character["crafting"],
-                        deception_prof=character["deception"],
-                        diplomacy_prof=character["diplomacy"],
-                        intimidation_prof=character["intimidation"],
-                        medicine_prof=character["medicine"],
-                        nature_prof=character["nature"],
-                        occultism_prof=character["occultism"],
-                        perception_prof=character["perception"],
-                        performance_prof=character["performance"],
-                        religion_prof=character["religion"],
-                        society_prof=character["society"],
-                        stealth_prof=character["stealth"],
-                        survival_prof=character["survival"],
-                        thievery_prof=character["thievery"],
-                        lores=lore,
-                        feats=character["feats"],
-                        key_ability=character["key_ability"],
-                        # attacks=attacks,
-                        # spells=spells,
-                        # resistance=resistance,
-                        eidolon=character["eidolon"],
-                        partner=character["partner"],
+                        humanity=character["humanity"],
+                        current_luck=character["stats"]["luck"]["value"],
+                        stats=character["stats"],
+                        skills=character["skills"],
+                        attacks=character["attacks"],
+                        armor=character["armor"],
+                        cyber=character["cyber"],
+                        net=character["net"],
+                        # Functional Stuff
+                        macros={},
+                        bonuses={},
                         pic=image,
                     )
                     session.add(new_char)
@@ -250,6 +152,9 @@ async def red_g_sheet_character_import(ctx: discord.ApplicationContext, char_nam
                 "Unnamed: 5": "f",
                 "Unnamed: 6": "g",
                 "Unnamed: 7": "h",
+                "Unnamed: 8": "i",
+                "Unnamed: 9": "j",
+                "Unnamed: 10": "k",
             },
             inplace=True,
         )
@@ -258,15 +163,18 @@ async def red_g_sheet_character_import(ctx: discord.ApplicationContext, char_nam
         return False
 
     character = {
-        "name": df.b[0],
+        "name": df.b[1],
         "level": int(df.d[0]),
         "active": True,
-        "role": df.b[1],
-        "hp": df.d[1],
+        "role": df.b[3],
+        "hp": df.d[2],
+        "humanity": df.d[11],
+        "cyber": {},
+        "net": {},
     }
     stats = {}
 
-    for i in range(2, 8):
+    for i in range(4, 10):
         try:
             title = str(df.a[i])
             title = title.split(" ")[0].lower()
@@ -286,160 +194,60 @@ async def red_g_sheet_character_import(ctx: discord.ApplicationContext, char_nam
     character["stats"] = stats
 
     skills = {}
-
-    feats = ""
-    if character["UI"] == True:
-        feats += "Untrained Improvisation, "
-    character["feats"] = feats
-    spells = {}
-    attacks = {}
-    items = []
-    resistances = {"resist": {}, "weak": {}, "immune": {}}
-
-    lore = ""
-    for i in range(26, 30):
-        print(df.d[i])
-        if type(df.e[i]) == str and type(df.d[i]) == str:
-            string = f"{df.d[i]}, {Interpreter[df.e[i]]}; "
-            lore += string
-    character["lore"] = lore
-
-    for i in range(31, (len(df.a) - 1)):
-        name = df.a[i]
-        # print(name)
-        if name != numpy.nan:
-            lookup_data = await spell_lookup(name)
-            if lookup_data[0] is True:
-                tradition = df.c[i]
-                # print(tradition)
-                match tradition:  # noqa
-                    case "Arcane":
-                        ability = Interpreter[df.f[19]]
-                        proficiency = Interpreter[df.e[19]]
-                    case "Divine":
-                        ability = Interpreter[df.f[20]]
-                        proficiency = Interpreter[df.e[20]]
-                    case "Occult":
-                        ability = Interpreter[df.f[21]]
-                        proficiency = Interpreter[df.e[21]]
-                    case "Primal":
-                        ability = Interpreter[df.f[22]]
-                        proficiency = Interpreter[df.e[22]]
-                    case _:
-                        ability = "cha"
-                        proficiency = 0
-
-                spell = {
-                    "level": int(df.b[i]),
-                    "tradition": tradition,
-                    "ability": ability,
-                    "proficiency": proficiency,
-                    "type": lookup_data[1].type,
-                    "save": lookup_data[1].save,
-                    "damage": lookup_data[1].damage,
-                    "heightening": lookup_data[1].heightening,
-                }
-                spells[name] = spell
-
-    for i in range(31, (len(df.e) - 1)):
-        if df.e[i] == "Name" and df.f[i] is not numpy.nan:
-            print(df.e[i])
+    for i in range(11, len(df.a)):
+        print(df.a[i])
+        if type(df.a[i]) == str:
             try:
-                # print(df.f[i])
+                skill_name = str(df.a[i]).lower()
+                skill_name = skill_name.strip()
+                level = int(df.b[i])
+                stat = str(df.c[i]).lower()
+                value = stats[stat]["value"] + level
+
+                item = {"value": value, "stat": stat, "base": level}
+                skills[skill_name] = item
+                print(item)
+            except Exception as e:
+                logging.error(f"red-g-sheet-import: {e}, {i}")
+    character["skills"] = skills
+    print(skills)
+
+    attacks = {}
+    for i in range(11, len(df.e)):
+        print(df.e[i], df.f[i], df.g[i], df.h[i])
+        if type(df.e[i]) == str:
+            if df.e[i] == "Name:":
+                print(df.e[i], str(df.f[i]))
                 try:
-                    potency = int(df.f[i + 3])
-                except Exception:
-                    potency = 0
-                die_num = 1
-                # print(type(df.f[i + 4]))
-                # print(df.f[i + 4])
-                if Interpreter[df.f[i + 4]] == "striking":
-                    die_num = 2
-                elif Interpreter[df.f[i + 4]] == "greaterStriking":
-                    die_num = 3
-                elif Interpreter[df.f[i + 4]] == "majorStriking":
-                    die_num = 4
-                if df.f[i + 8] is not numpy.nan:
-                    parsed_traits = df.f[i + 8].split(",")
-                else:
-                    parsed_traits = []
-                if type(df.f[i + 9]) == str:
-                    dmg_type = df.f[i + 9]
-                else:
-                    dmg_type = "Bludgeoning"
+                    dmg_string = f"{df.f[i+2]}{df.g[i+2]}"
+                    name = str(df.f[i])
+                    attack_data = {
+                        "skill": str(df.f[i + 1]).lower(),
+                        "type": str(df.h[i + 1]).lower(),
+                        "dmg": dmg_string,
+                        "hands": int(df.f[i + 3]),
+                        "rof": int(df.f[i + 4]),
+                        "attk_bonus": int(df.f[i + 5]),
+                        "autofire": Interpreter[df.f[i + 6]],
+                        "autofire_ammt": int(df.h[i + 6]),
+                        "attach": str(df.f[i + 7]),
+                    }
+                    attacks[name] = attack_data
+                    print(attack_data)
+                except Exception as e:
+                    logging.error(f"red-g-sheet-import: {e}, {i}")
+    print(attacks)
+    character["attacks"] = attacks
 
-                attack_data = {
-                    "display": df.f[i],
-                    "name": df.f[i + 1],
-                    "prof": df.f[i + 2].lower(),
-                    "pot": potency,
-                    "str": Interpreter[df.f[i + 4]],
-                    "runes": [],
-                    "die_num": die_num,
-                    "die": df.f[i + 5],
-                    "crit": "*2",
-                    "stat": Interpreter[df.f[i + 7]],
-                    "dmg_type": dmg_type,
-                    "attk_stat": Interpreter[df.f[i + 6]],
-                    "traits": parsed_traits,
-                }
-                edited_attack = await attack_lookup(attack_data, character)
-
-                double_attacks = False
-                # Check for two handed and fatal
-                for trait in edited_attack["traits"]:
-                    print(trait)
-                    if "fatal-aim" in trait:
-                        double_attacks = True
-                        parsed_trait = trait.split("-")
-                        fatal_die = parsed_trait[2]
-                        attack_one = edited_attack.copy()
-                        attack_two = edited_attack.copy()
-                        trait_list = attack_one["traits"]
-                        trait_copy = trait_list.copy()
-                        for x, i in enumerate(trait_list):
-                            if i == trait:
-                                trait_copy[x] = f"fatal-{fatal_die}"
-
-                        attack_one["traits"] = trait_copy
-                        attack_one["display"] = f"{edited_attack['display']} (2H)"
-
-                        trait_copy = []
-                        for i in trait_list:
-                            if i != trait:
-                                trait_copy.append(i)
-
-                        attack_two["display"] = f"{edited_attack['display']} (1H)"
-                        attack_two["traits"] = trait_copy
-                    if "two-hand" in trait:
-                        double_attacks = True
-                        parsed_trait = trait.split("-")
-                        attk_2_die = parsed_trait[2]
-                        attack_one = edited_attack.copy()
-                        attack_two = edited_attack.copy()
-                        attack_one["display"] = f"{edited_attack['display']} (2H)"
-                        attack_one["die"] = attk_2_die
-                        attack_two["display"] = f"{edited_attack['display']} (1H)"
-
-                if double_attacks:
-                    attacks[attack_one["display"]] = attack_one
-                    attacks[attack_two["display"]] = attack_two
-                else:
-                    attacks[edited_attack["display"]] = edited_attack
-
-            except Exception:
-                pass
-    # print(attacks)
-
-    # print("Getting Items")
-    for i in range(31, (len(df.h) - 1)):
-        # print(i)
-        # print(df.h[i])
-        if df.h[i] != numpy.nan:
-            # print(df.h[i])
-            items.append(df.h[i])
-
-    return character, spells, attacks, items, resistances
+    armor = {}
+    for i in range(12, 14):
+        if type(df.i[i]) == str:
+            location = str(df.i[i])
+            sp = df.j[1]
+            penalty = df.k[i]
+        armor[location] = {"sp": sp, "penalty": penalty}
+    print(armor)
+    return character
 
 
 async def gs_npc_skill_calc(skill_mod, level, stat_mod):
