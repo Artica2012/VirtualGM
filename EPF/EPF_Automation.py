2a8da78f478557ee8da3c8a081e5c4d4a4bd06db
Artica2012 <schneiaj@gmail.com>
2023-11-01 21:15:05 -0500

    Bugfixes  - Fixed typo in time advance method
     EPF
      - Treat wounds now appropriately updates the tracker.

--- EPF/EPF_Automation.py
+++ EPF/EPF_Automation.py
@@ -700,6 +700,9 @@ async def treat_wounds(ctx, character, target, dc, modifier, engine, guild=None)
             time = 60
         await Target_Model.set_cc("Wounds Treated", False, time, "Minute", True)
 
+    Tracker_Model = await get_tracker_model(ctx, None, guild=guild, engine=engine)
+    await Tracker_Model.update_pinned_tracker()
+
     embed = discord.Embed(title="Treat Wounds", description=output_string)
     embed.set_thumbnail(url=Character_Model.pic)
 
