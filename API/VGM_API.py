9b7b0c3bd4456fe91f4f9321073dd1029d48769e
Artica2012 <schneiaj@gmail.com>
2024-01-11 00:22:50 -0600

    Now serving an API via FastAPI within the bot

--- API/VGM_API.py
+++ API/VGM_API.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 import uvicorn
 import API.endpoints as endpoints
 
+
 app = FastAPI()
 app.include_router(endpoints.router)
 
 
 def start_uvicorn(loop):
-    config = uvicorn.Config(app, loop=loop)
+    config = uvicorn.Config(app, loop=loop, port=8080, host="0.0.0.0")
     server = uvicorn.Server(config)
     loop.create_task(server.serve())
