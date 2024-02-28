06312c6873c40886bcafdbd3485672ba36d5d89d
Artica2012 <schneiaj@gmail.com>
2024-02-27 19:53:00 -0600

    Added https into cors.

--- API/VGM_API.py
+++ API/VGM_API.py
@@ -14,9 +14,9 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "https://corsproxy.io",
+    "http://localhost:5173",
+    "https://localhost:5173",
     "https://virtualgm.dev",
-    "shiny-twilight-83afc1.netlify.app",
 ]
 
 app.add_middleware(
